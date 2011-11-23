b0d682337bb495b4777509467d338397e4d41a55
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2011-11-23 13:51:41 -0500

    Counter reset

--- sess_img_order.js
+++ sess_img_order.js
@@ -1,5 +1,12 @@
-i = 0
-db.sessions.find().forEach( function(sess) { sess.images.forEach( function(img) { new_item = {'img':img, 'pos':i}; db.sessions.update( {'_id':sess._id}, {$push: {'images_new': new_item}} ); i+=1 } ) } )
+i = 0;
+db.sessions.find().forEach( function(sess) { db.sessions.update( {'_id':sess._id}, {$unset: {'images_new': 1}} ) } );
 
-db.sessions.find().forEach( function(sess) { db.sessions.update( {'_id':sess._id}, {$unset: {'images': 1}} ) } )
-db.sessions.find().forEach( function(sess) { db.sessions.update( {'_id':sess._id}, {$rename: {'images_new': 'images'}} ) } )
+i = 0;
+db.sessions.find().forEach( 
+	function(sess) 
+		{ 
+		i = 0;
+		sess.images.forEach( function(img) { new_item = {'img':img, 'pos':i}; db.sessions.update( {'_id':sess._id}, {$push: {'images_new': new_item}} ); i+=1 } ) } )
+		db.sessions.find().forEach( function(sess) { db.sessions.update( {'_id':sess._id}, {$unset: {'images': 1}} ) } )
+		db.sessions.find().forEach( function(sess) { db.sessions.update( {'_id':sess._id}, {$rename: {'images_new': 'images'}} ) 
+	});
