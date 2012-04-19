ac56ec294bd861f2b7a1d5eb770d1af32075773f
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2012-04-19 16:54:13 -0400

    Basic structure for object oriented design

--- items_in_collection.py
+++ items_in_collection.py
@@ -1,16 +1,22 @@
 import pymongo
+import bson
+import sys
 
-
-class item_type():
-	def __init__(self, type_name):
-		self.type_name = type_name
-
-class items_in_collection():
-	def __init__(self, item_type_string):
+class ItemsInCollection():
+	def __init__(self, item_type_string, db, root, session_key=None):
 		self.item_type = item_type_string
-		pass
+		self.db = db
+		self.root = root
+
+		if not session_key == None:
+			self.SetSession(session_key)
 
 	def SetSession(self, session_key):
+		# Look for the session 
+
+		# Assign if the session is around else return false
+		
+		
 		pass
 	
 	def SetItemType(self, item_type_string):
@@ -23,6 +29,18 @@ class items_in_collection():
 		pass
 
 
-class images_in_collection(items_in_collection):
-	def __init__(self):
-			items_in_collection.__init__(self, "images")
+class Attachments(ItemsInCollection):
+	def __init__(self, db, root):
+		items_in_collection.__init__(self, "attachments")
+
+	def Insert(self, item):
+		pass
+	
+	def Delete(self, item_key):
+		
+	def List(self):
+ 
+
+
+
+
