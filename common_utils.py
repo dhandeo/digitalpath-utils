59ae8f4b0585e3285813c4c127f6ac98f24cb8c9
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2012-04-25 17:54:20 -0400

    Soft matching for image utilities

--- common_utils.py
+++ common_utils.py
@@ -5,7 +5,7 @@ import pymongo
 import sys
 import bson.objectid as oid
 
-def get_object_in_collection(col, key, debug=False):
+def get_object_in_collection(col, key, debug=False, soft=False):
 
 	# Find if the key is id
 	obj = None
@@ -16,7 +16,6 @@ def get_object_in_collection(col, key, debug=False):
 	except:
 		if debug:
 			print "_ID did not work",
-		pass
 	
 	# else check in the 
 	obj = col.find_one({'name':key})
@@ -25,12 +24,31 @@ def get_object_in_collection(col, key, debug=False):
 		if debug:
 			print 'Name works',
 		return obj
+
+	if soft == True:
+		# dig through the images in the session
+		print "Being softer"		
+
+		found = 0 
+
+		# Get a list of all image names and 
+		for animage in col.find():
+			if key in animage['name']: 
+				print '   Matched :', animage['name']
+				found = found + 1
+				obj = animage
+
+		if found == 1:
+			if debug:
+				print 'Soft matching worked ..'
+			return obj
+			
+		elif found > 1:
+			print 'Cannot work with multiple matches ..'
 	
-	# Return None or return false
-	else:
-		if debug:
-			print "Nothing worked .."
-		return None
+	if debug:
+		print "Nothing worked .."
+	return None
 
 def get_session_from_key(db, key, debug=False):
 	pass	
