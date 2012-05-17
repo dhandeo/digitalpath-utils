4f56d89d08c148252d32015fede9924fd2fe4aab
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2012-05-17 12:26:27 -0400

    Rename

--- attachments.py
+++ attachments.py
@@ -1,104 +1,97 @@
-# Follow from main but example usage
-# python add_groups.py localhost slideatlas
+# vim: set tabstop=2 shiftwidth=2 expandtab
 
-# adds or delets attachments from sessions 
-# The attachment must already exist
+from items_in_collection import *
+from common_utils import *
+import os
 
-import pymongo
-import sys
-import bson.objectid as oid 
-from common_utils import get_object_in_collection
-
-def attachment_in_session(mongodb, cmd, session_key, attachment_key, force=False):
-
-	# Verify that the session exists
-	session = get_object_in_collection(mongodb['sessions'], session_key)
-	attachment = get_object_in_collection(mongodb['attachments'], attachment_key)
-	
-	if attachment == None or session == None:
-		print "Could not find objects for ", attachment_key, " or ", session_key
-		return
-	
-	if cmd == "add":
-		# Update the can_see
-		print "Adding .."
-		attachments = session['attachments']
-
-		max = 0		
-		for anattachment in attachments:
-			#print anattachment
-			if anattachment['pos'] > max:
-				max = anattachment['pos']
-
-		new_attachment = {}
-		new_attachment[u'ref'] = attachment['_id']
-		new_attachment[u'pos'] = max + 1 
-		new_attachment[u'hide'] = False
-
-		attachments.append(new_attachment)
-		mongodb['sessions'].update({'_id': session['_id']}, {'$set':{'attachments': attachments}})
-
-		for a in attachments:
-			print a
-		
-		pass
-	
-	elif cmd == "del":
-		print "Deleting .."
-		
-		# verify if the attachment is listed in the session 
-		found = False
-		attachments = session['attachments']
-		
-		for anattachment in attachments:
-			#print anattachment
-			if anattachment['ref'] == attachment['_id']:
-				print 'Found'  
-				found = True
-				print anattachment
-				index = attachments.index(anattachment)
-				attachments[index]['hide'] = True
-		
-		if found == False:
-			print "attachment not found in session"
-			
-		print attachments
-		mongodb['sessions'].update({'_id': session['_id']}, {'$set':{'attachments': attachments}})
-
-def copy_attachment(from_mongodb, from_attachment, from_session):
-	pass
-
-
-# Main to accept command line and do the operation on attachments. 
+# Main to accept command line and do the operation on images.
 if __name__ == '__main__':
-	# get the command line arguments
-	if len(sys.argv) < 3:
-		print 'incorrect usage' 
-		print 'correct use: python attachments.py server database command session attachment [force]' 
-		sys.exit(0)
+    # get the command line arguments
+    if len(sys.argv) < 5:
+        print 'incorrect usage'
+        print 'correct use: python test_attachments.py server database command session_key [path_to_attachment] [output_path]'
+        sys.exit(0)
 
-	server   = sys.argv[1]
-	database = sys.argv[2]
-	command  = sys.argv[3]
-	attachment    = sys.argv[4]
-	session  = sys.argv[5]
-	
-	# Cleanup if force parameter specified
-	try:	
-		force = int(sys.argv[6]) 
-	except:
-		force = 0
+    server = sys.argv[1]
+    database = sys.argv[2]
+    command = sys.argv[3]
+    session_key = sys.argv[4]
 
-	try:
-		# Try opening the database	
-		conn = pymongo.Connection(server); 
-		mongodb = conn[database]
-	
-	except:
-		print "Error opening ", database, " at ", server
-	if force:
-		print "Using Force ..", 
+    # Try if attachment path is specified
+    try:
+        attachment_path = sys.argv[5]
+    except:
+        attachment_path = False
 
-	attachment_in_session(mongodb, command, session, attachment, force)
+    # Try if output path is specified
+    try:
+        output_path = sys.argv[6]
+    except:
+        output_path = False
 
-	sys.exit(0)
+    try:
+        # Try opening the database
+        conn = pymongo.Connection(server)
+        mongodb = conn[database]
+
+    except:
+        print "Error opening ", database, " at ", server
+
+    if attachment_path:
+        print "Attachment path specified"
+    else:
+        if command != "list" and command != "flush":
+            print "Attachment path required"
+            sys.exit(0)
+
+    # Get session from session key
+
+    session = get_object_in_collection(mongodb["sessions"], session_key)
+
+    if session == None:
+        print "Couldnot locate session from ", session_key
+        sys.exit(0)
+
+    # Create the object with that mongodb
+    ac = Attachments(mongodb, session)
+
+    # Open the file if insert
+    if command == "insert":
+        try:
+            fin = open(attachment_path, "r+")
+            attachdata = fin.read()
+        except:
+            print "Cannot open attachment"
+
+        [head, tail] = os.path.split(attachment_path)
+        ac.Insert(attachdata, tail)
+
+    elif command == 'get':
+        if not output_path:
+            print 'Output path is required'
+            sys.exit(0)
+        else:
+            fout = open(output_path, 'w+')
+            fout.write(ac.Get(attachment_path).read())
+
+    elif command == "delete":
+        ac.Delete(attachment_path)
+        print "Find and delete"
+
+    elif command == "list":
+        ac.List()
+
+    elif command == "flush":
+        resp = raw_input('Are you sure you want to flush ?')
+
+        if resp == 'y' or resp == 'Y':
+            ac.Flush()
+
+    elif 1:
+        print "Unknown command : ", command
+        sys.exit(0)
+
+    # Get the id from the session key
+    #image_in_session(mongodb, command, session, image, force)
+    print "Done .."
+    sys.exit(0)
